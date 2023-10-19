5619f7e4044ba3ce987f7537e014891e5a5e96d9
Remsiii <remdab17@htl-kaindorf.at>
2023-10-19 21:03:08 +0200

    added shadcn to react and did some testing

--- arb_app/app/layout.tsx
+++ arb_app/app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Inter as FontSans } from "next/font/google"
+import { ThemeProvider } from "@/components/theme-provider"
 
-const inter = Inter({ subsets: ['latin'] })
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +22,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={fontSans.variable}>
+
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            </ThemeProvider>
+
+            
+            </body>
     </html>
   )
 }
+
+
