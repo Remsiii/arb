f60f8877ebb82b198959f6e19b565b227ba90e92
Remsiii <remdab17@htl-kaindorf.at>
2023-10-23 19:58:50 +0200

    added some dependecies

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -26,18 +26,16 @@ export default function RootLayout({
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </head>
       <body className={fontSans.variable}>
-
-          <ThemeProvider
+      <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
-            {children}
-            </ThemeProvider>
+        {children}
+        </ThemeProvider>
 
-            
-            </body>
+      </body>
     </html>
   )
 }
